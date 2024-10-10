027689c403e791ad2adaed803d463a3e07056ecf
Iván Barberá <ivanbarbera93@gmail.com>
2024-10-10 01:26:19 -0300

    Edit slides and recompile

--- target_example_m2.R
+++ target_example_m2.R
@@ -1,4 +1,4 @@
-target <- 0
+target <- 0 # log posterior
 target <- target + dnorm(alpha, mean = alpha_mu, sd = alpha_sigma, log = T)
 target <- target + dnorm(beta, mean = beta_mu, sd = beta_sigma, log = T)
 target <- target + sum(dbinom(y, prob = theta, size = 1, log = T))
